29c30b88a577640e87636171a0d88ec053b11c6e
Anton Delorme <anton.delorme@outlook.com>
2018-02-02 18:58:52 +0100

    Fix replays show wrong game quality
    Fixed it to show the proper quality. Added '%' for clarity.
    Fixes #738

--- src/main/java/com/faforever/client/rating/JSkillsRatingService.java
+++ src/main/java/com/faforever/client/rating/JSkillsRatingService.java
@@ -38,6 +38,6 @@ public class JSkillsRatingService implements RatingService {
           ));
           return team;
         })
-        .collect(Collectors.toList()));
+        .collect(Collectors.toList())) * 100;
   }
 }
