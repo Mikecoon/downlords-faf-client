bef960cb1ae33f485ffbe700b771794d8310af2a
Alexander von Trostorff <alexander.von.trostorff@gmail.com>
2018-02-24 23:57:29 +0100

    Remake create game button
    Gives the create game button the correct implementation
    Fixes #720

--- src/main/java/com/faforever/client/game/CreateGameController.java
+++ src/main/java/com/faforever/client/game/CreateGameController.java
@@ -394,4 +394,14 @@ public class CreateGameController implements Controller<Pane> {
   public void onContentPaneClicked(MouseEvent event) {
     event.consume();
   }
+
+  public boolean selectMap(String mapFolderName) {
+    Optional<MapBean> mapBeanOptional = mapListView.getItems().stream().filter(mapBean -> mapBean.getFolderName().equalsIgnoreCase(mapFolderName)).findAny();
+    if (!mapBeanOptional.isPresent()) {
+      return false;
+    }
+    mapListView.getSelectionModel().select(mapBeanOptional.get());
+    mapListView.scrollTo(mapBeanOptional.get());
+    return true;
+  }
 }
